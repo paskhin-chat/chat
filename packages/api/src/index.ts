8019f7266c05b5bb460d95e311844988729e42c8
Dmitry Paskhin <d.pasxin@gmail.com>
2023-05-14 02:11:03 +0400

    refactor(config): Refactored config logic (#4)

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,17 +2,13 @@ import { NestFactory } from '@nestjs/core';
 
 import { PrismaService } from './prisma/prisma.service';
 import { AppModule } from './app.module';
-import { ConfigService } from './config/config.service';
 
 (async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
 
   const prismaService = app.get(PrismaService);
-  const configService = app.get(ConfigService);
-
-  app.setGlobalPrefix(configService.apiPrefix);
 
   prismaService.enableShutdownHooks(app);
 
-  await app.listen(configService.apiPort);
+  await app.listen(3_002);
 })();
