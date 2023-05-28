813eb9643ef9f18df8c21938b3c0edde1e3cafbd
Dmitry Paskhin <d.pasxin@gmail.com>
2023-05-28 23:33:36 +0400

    feat(config): Add environment configs

--- packages/config/src/main/index.ts
+++ packages/config/src/main/index.ts
@@ -1,2 +1,5 @@
-export * as config from './gen/config';
+export * as prodConfig from './gen/prod-config';
+export * as devConfig from './gen/dev-config';
+export * as testConfig from './gen/test-config';
+
 export type { IConfig } from './bin';
